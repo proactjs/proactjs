9e5ba9c8c32847cfc89aec9421b1a33d8574e0cd
meddle <meddlesmerch@gmail.com>
2014-12-07 02:41:59 +0200

    Spec for 'fromInterval'

---
+++ spec/unit/objects/from_invoke.spec.js
@@ -0,0 +1,28 @@
+'use strict';
+
+describe('ProAct.fromInvoke', function () {
+
+  beforeEach(function () {
+    jasmine.Clock.useMock();
+  });
+  it ('creates a ProAct.Stream', function () {
+    var stream = ProAct.fromInvoke(1000, function () {});
+
+    expect(stream instanceof ProAct.Stream).toBe(true);
+  });
+
+  it ('creates a ProAct.Stream emitting the result of the passed "func" argument on every "interval" milliseconds', function () {
+    var stream = ProAct.fromInvoke(1000, function () {
+      return 5;
+    });
+    stream.on(resultListener);
+
+    expect(res).toEqual([]);
+
+    jasmine.Clock.tick(1000);
+    expect(res).toEqual([5]);
+
+    jasmine.Clock.tick(1000);
+    expect(res).toEqual([5, 5]);
+  });
+});
