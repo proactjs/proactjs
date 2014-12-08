cbca8ea283aafb10e3c4265fd458e5f83f002028
meddle <meddlesmerch@gmail.com>
2014-12-09 00:05:01 +0200

    Documentation and bugfixes for helper methods.

--- src/js/objects/prob.js
+++ src/js/objects/prob.js
@@ -108,6 +108,29 @@ function closed () {
 }
 ProAct.closed = P.never = closed;
 
+/**
+ * Creates a {@link ProAct.Stream}, which emits the passed "value" once and then closes.
+ * <p>Example:</p>
+ * <pre>
+    var stream = ProAct.timeout(1000, 7);
+    stream.on(function (v) {
+      console.log(v);
+    });
+
+   // This will print '7' after 1s and will close.
+
+ * </pre>
+ *
+ * @method timeout
+ * @memberof ProAct
+ * @static
+ * @param {Number} timeout
+ *      The time to wait (in milliseconds) before emitting the <i>value</i> and close.
+ * @param {Object} value
+ *      The value to emit.
+ * @return {ProAct.Stream}
+ *      A {@link ProAct.Stream} instance.
+ */
 function timeout (timeout, value) {
   var stream = P.stream();
 
@@ -120,6 +143,29 @@ function timeout (timeout, value) {
 }
 ProAct.timeout = ProAct.later = timeout;
 
+/**
+ * Creates a {@link ProAct.Stream}, which emits the passed "value" over and over again at given time interval.
+ * <p>Example:</p>
+ * <pre>
+    var stream = ProAct.interval(1000, 7);
+    stream.on(function (v) {
+      console.log(v);
+    });
+
+   // This will print one number on every 1s and the numbers will be 7,7,7,7,7....
+
+ * </pre>
+ *
+ * @method interval
+ * @memberof ProAct
+ * @static
+ * @param {Number} interval
+ *      The time in milliseconds on which the <i>value</i> will be emitted.
+ * @param {Object} value
+ *      The value to emit.
+ * @return {ProAct.Stream}
+ *      A {@link ProAct.Stream} instance.
+ */
 function interval (interval, value) {
   var stream = P.stream();
 
@@ -131,22 +177,48 @@ function interval (interval, value) {
 }
 ProAct.interval = interval;
 
+/**
+ * Creates a {@link ProAct.Stream}, which emits values of the passed <i>vals</i> array on the passed <i>interval</i> milliseconds.
+ * <p>
+ *  When every value is emitted through the stream it is closed.
+ * <p>
+ * <p>Example:</p>
+ * <pre>
+    var stream = ProAct.seq(1000, [4, 5]);
+    stream.on(function (v) {
+      console.log(v);
+    });
+
+   // This will print one number on every 1s and the numbers will be 4 5 and the stream will be closed.
+
+ * </pre>
+ *
+ * @method seq
+ * @memberof ProAct
+ * @static
+ * @param {Number} interval
+ *      The time in milliseconds on which a value of the passed <i>vals</i> array will be emitted.
+ * @param {Array} vals
+ *      The array containing the values to be emitted on the passed <i>interval</i>.
+ * @return {ProAct.Stream}
+ *      A {@link ProAct.Stream} instance.
+ */
 function seq (interval, vals) {
   var stream = P.stream(),
       operation;
 
   if (vals.length > 0) {
     operation = function () {
-      var value = vals.unshift();
+      var value = vals.shift();
       stream.trigger(value);
 
       if (vals.length === 0) {
         stream.close();
       } else {
-        window.setTimeout(operation, timeout);
+        window.setTimeout(operation, interval);
       }
     };
-    window.setTimeout(operation, timeout);
+    window.setTimeout(operation, interval);
   }
 
   return stream;
@@ -169,14 +241,13 @@ ProAct.seq = seq;
 
  * </pre>
  *
- * @method fromInvoke
+ * @method interval
  * @memberof ProAct
  * @static
  * @param {Number} interval
- *      The interval on which <i>func</i> will be called and its returned value will
- *      be triggered into the stream.
- * @param {Function} func
- *      The function to invoke in order to get the value to trigger into the stream.
+ *      The time in milliseconds on which a value of the passed <i>vals</i> array will be emitted.
+ * @param {Array} vals
+ *      The array containing the values to be emitted on the passed <i>interval</i>.
  * @return {ProAct.Stream}
  *      A {@link ProAct.Stream} instance.
  */
@@ -226,12 +297,18 @@ ProAct.repeat = repeat;
  *      A {@link ProAct.Stream} instance.
  */
 function fromInvoke (interval, func) {
-  var stream = P.stream();
+  var stream = P.stream(), id;
 
-  window.setInterval(function () {
+  id = window.setInterval(function () {
     var value = func.call();
 
-    stream.trigger(value);
+    if (value !== ProAct.close) {
+      stream.trigger(value);
+    } else {
+      stream.close();
+      window.clearInterval(id);
+    }
+
   }, interval);
 
   return stream;
