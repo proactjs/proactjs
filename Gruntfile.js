72e969f507cdd16ba7004698001cc6b57608face
meddle <meddlesmerch@gmail.com>
2014-12-16 10:00:13 +0200

    Grunt yuidoc.

--- Gruntfile.js
+++ Gruntfile.js
@@ -82,6 +82,20 @@ module.exports = function(grunt) {
       }
     },
 
+    pkg: grunt.file.readJSON('package.json'),
+    yuidoc: {
+      compile: {
+        name: '<%= pkg.name %>',
+        description: '<%= pkg.description %>',
+        version: '<%= pkg.version %>',
+        url: '<%= pkg.homepage %>',
+        options: {
+          paths: 'src',
+          outdir: 'doc'
+        }
+      }
+    },
+
     todo: {
       options: {
         verbose: true,
