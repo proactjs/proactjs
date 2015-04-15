9ecf299ea7d0ef5376a34d2e512aa98870e351af
meddle <meddlesmerch@gmail.com>
2015-04-15 09:01:22 +0300

    SubscribableStream is being documented.

--- src/js/streams/subscribable_stream.js
+++ src/js/streams/subscribable_stream.js
@@ -1,3 +1,45 @@
+/**
+ * @module proact-streams
+ */
+
+/**
+ * <p>
+ *  Constructs a `ProAct.SubscribableStream`. This is a `Stream` that has a custom `subscribe` function, used to subscribe to a source.
+ * </p>
+ *
+ * This can be used to stream sources like browser events. The stream is lazy, when there are no listeners to it,
+ * it is not subscribed to the source, on the first listener it is subscribed, when every listener is unsubscibed, it is unsubscribed.
+ *
+ * <p>
+ *  `ProAct.SubscribableStream` is part of the `proact-streams` module of ProAct.js.
+ * </p>
+ *
+ * @class ProAct.SubscribableStream
+ * @constructor
+ * @extends ProAct.SubscribableStream
+ * @param {Function} subscribe
+ *      A function used to subscribe to a source, when the first listener to this stream is attached.
+ * @param {String} queueName
+ *      The name of the queue all the updates should be pushed to.
+ *      <p>
+ *        If this parameter is null/undefined the default queue of
+ *        {{#crossLink "ProAct/flow:property"}}{{/crossLink}} is used.
+ *      </p>
+ *      <p>
+ *        If this parameter is not a string it is used as the
+ *        <i>source</i>.
+ *      </p>
+ * @param {ProAct.Actor} source
+ *      A default source of the stream, can be null.
+ *      <p>
+ *        If this is the only one passed argument and it is a number - it becomes the size of the buffer.
+ *      </p>
+ * @param {Array} transforms
+ *      A list of transformation to be used on all incoming chages.
+ *      <p>
+ *        If the arguments passed are two and this is a number - it becomes the size of the buffer.
+ *      </p>
+ */
 function SubscribableStream (subscribe, queueName, source, transforms) {
   P.S.call(this, queueName, source, transforms);
 
