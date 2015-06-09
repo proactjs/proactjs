a9fa636b2045b025cd91d9707202c665c316d1b3
meddle <meddlesmerch@gmail.com>
2015-06-10 00:06:11 +0300

    ProAct.Flow in YUIDoc.

--- src/js/flow/flow.js
+++ src/js/flow/flow.js
@@ -13,8 +13,8 @@
  *  between the reactive properties. The action flow puts on motion the data flow in the reactive
  *  ecosystem. Every change on a property triggers an action flow, which triggers the data flow.
  * </p>
- *  ProAct.Flow is inspired by the [Ember's Backburner.js]{@link https://github.com/ebryn/backburner.js}.
- *  The defferences are the priority queues and some other optimizations like the the 'once' argument of the {@link ProAct.Queue#go} method.
+ *  ProAct.Flow is inspired by the Ember's Backburner.js (https://github.com/ebryn/backburner.js).
+ *  The defferences are the priority queues and some other optimizations like the the 'once' argument of the {{#crossLink "ProAct.Queue/go:method"}}{{/crossLink}} method.
  *  It doesn't include debouncing and timed defer of actions for now.
  * <p>
  *  ProAct.Flow is used to solve many of the problems in the reactive programming, for example the diamond problem.
@@ -23,14 +23,15 @@
  *  It can be used for other purposes too, for example to run rendering in a rendering queue, after all of the property updates.
  * </p>
  * <p>
- *  ProAct.Flow, {@link ProAct.Queues} and {@link ProAct.Queue} together form the ActionFlow module of ProAct.
+ *  `ProAct.Flow`, {{#crossLink "ProAct.Queues"}}{{/crossLink}} and {{#crossLink "ProAct.Queue"}}{{/crossLink}} together form the `proact-flow` module of ProAct.
  * </p>
  *
  * TODO ProAct.Flow#start and ProAct.Flow#stop are confusing names - should be renamed to something like 'init' and 'exec'.
  *
+ * @constructor
  * @class ProAct.Flow
  * @param {Array} queueNames
- *      Array with the names of the sub-queues of the {@link ProAct.Queues}es of the flow. The size of this array determines
+ *      Array with the names of the sub-queues of the {{#crossLink "ProAct.Queues"}}{{/crossLink}}es of the flow. The size of this array determines
  *      the number of the sub-queues.
  * @param {Object} options
  *    Various options for the ProAct.Flow.
@@ -39,10 +40,8 @@
  *      <li>start - A callback that will be called every time when the action flow starts.</li>
  *      <li>stop - A callback that will be called every time when the action flow ends.</li>
  *      <li>err - A callback that will be called if an error is thrown in the action flow.</li>
- *      <li>flowInstance - Options object for the current flow instance. The flow instances are @{link ProAct.Queues}es.</li>
+ *      <li>flowInstance - Options object for the current flow instance. The flow instances are of the class {{#crossLink "ProAct.Queues"}}{{/crossLink}}.</li>
  *    </ul>
- * @see {@link ProAct.Queues}
- * @see {@link ProAct.Queue}
  */
 ProAct.Flow = P.F = function (queueNames, options) {
   this.setQueues(queueNames);
@@ -71,29 +70,28 @@ P.F.prototype = {
   /**
    * Reference to the constructor of this object.
    *
-   * @memberof ProAct.Flow
-   * @instance
-   * @constant
-   * @type {Object}
-   * @default ProAct.Flow
+   * @property constructor
+   * @type ProAct.Flow
+   * @final
+   * @for ProAct.Flow
    */
   constructor: ProAct.Flow,
 
   /**
-   * Puts the ProAct.Flow in running mode, meaning actions can be defered in it.
+   * Puts the `ProAct.Flow` in running mode, meaning actions can be defered in it.
    * <p>
-   *  It creates a new flow instance - instance of {@link ProAct.Queues} and
+   *  It creates a new flow instance - instance of {{#crossLink "ProAct.Queues"}}{{/crossLink}} and
    *  if there was a running instance, it is set to be the previous inctance.
    * </p>
    * <p>
-   *  If a <i>start</i> callback was passed when this ProAct.Flow was being created,
+   *  If a <i>start</i> callback was passed when this `ProAct.Flow` was being created,
    *  it is called with the new flow instance.
    * </p>
    * <p>
-   *  ProAct.Flow.begin is alias of this method.
+   *  `begin` is alias of this method.
    * </p>
    *
-   * @memberof ProAct.Flow
+   * @for ProAct.Flow
    * @instance
    * @method start
    */
@@ -120,7 +118,7 @@ P.F.prototype = {
    *  When a new <i>flowInstance</i> is created the updated list will be used.
    * </p>
    *
-   * @memberof ProAct.Flow
+   * @for ProAct.Flow
    * @instance
    * @method addQueue
    * @param {String} queueName
@@ -136,11 +134,11 @@ P.F.prototype = {
    *  When a new <i>flowInstance</i> is created the new list will be used.
    * </p>
    *
-   * @memberof ProAct.Flow
+   * @for ProAct.Flow
    * @instance
    * @method setQueues
    * @param {Array} queueNames
-   *      Array with the names of the sub-queues of the {@link ProAct.Queues}es of the flow.
+   *      Array with the names of the sub-queues of the {{#crossLink "ProAct.Queues"}}{{/crossLink}}es of the flow.
    *      The size of this array determines the number of the sub-queues.
    */
   setQueues: function (queueNames) {
@@ -152,11 +150,11 @@ P.F.prototype = {
 
   /**
    * Starts an action flow consisting of all the actions defered after the
-   * last call of {@link ProAct.Flow#start} and then stops the ProAct.Flow.
+   * last call of {{#crossLink "ProAct.Flow/start:method"}}{{/crossLink}} and then stops the `ProAct.Flow`.
    *
    * <p>
    *  If there is a current action flow instance, it is flushed, using the
-   *  {@link ProAct.Queues#go} method.
+   *  {{#crossLink "ProAct.Queues/go:method"}}{{/crossLink}} method.
    * </p>
    * <p>
    *  If there was aprevious flow instance, it is set to be the current one.
@@ -170,10 +168,10 @@ P.F.prototype = {
    *  and if you want to execute them and stop it, you call this method.
    * </p>
    * <p>
-   *  ProAct.Flow#end is an alias for this method.
+   *  `end` is an alias for this method.
    * </p>
    *
-   * @memberof ProAct.Flow
+   * @for ProAct.Flow
    * @instance
    * @method stop
    */
@@ -207,11 +205,9 @@ P.F.prototype = {
    * When the flow is paused actions that should be defered to be run in it
    * are skipped.
    *
-   * @memberof ProAct.Flow
+   * @for ProAct.Flow
    * @instance
    * @method pause
-   * @see {@link ProAct.Flow#push}
-   * @see {@link ProAct.Flow#pushOnce}
    */
   pause: function () {
     this.pauseMode = true;
@@ -221,10 +217,9 @@ P.F.prototype = {
    * Resumes the action flow if it is paused.
    * The flow becomes active again and actions can be pushed into it.
    *
-   * @memberof ProAct.Flow
+   * @for ProAct.Flow
    * @instance
    * @method resume
-   * @see {@link ProAct.Flow#pause}
    */
   resume: function () {
     this.pauseMode = false;
@@ -238,20 +233,16 @@ P.F.prototype = {
    *  that should be pushed to a flow in the <i>callback</i>.
    * </p>
    * <p>
-   *  ProAct.Flow#go and ProAct.Flow#flush are aliases of this method.
+   *  `go` and `flush` are aliases of this method.
    * </p>
    *
-   * @memberof ProAct.Flow
+   * @for ProAct.Flow
    * @instance
    * @method run
    * @param {Object} context
    *      The value of <i>this</i> bound to the <i>callback</i> when it is executed.
    * @param {Function} callback
-   *      The callback that will be invoked in a new running ProAct.Flow.
-   * @see {@link ProAct.Flow#start}
-   * @see {@link ProAct.Flow#stop}
-   * @see {@link ProAct.Flow#push}
-   * @see {@link ProAct.Flow#pushOnce}
+   *      The callback that will be invoked in a new running `ProAct.Flow`.
    */
   run: function (context, callback) {
     var options = this.options,
@@ -284,28 +275,24 @@ P.F.prototype = {
   },
 
   /**
-   * Checks if there is an active {@link ProAct.Queues} instance in this ProAct.Flow.
+   * Checks if there is an active {{#crossLink "ProAct.Queues"}}{{/crossLink}} instance in this `ProAct.Flow`.
    *
    * TODO This should be named 'isActive'.
    *
-   * @memberof ProAct.Flow
+   * @for ProAct.Flow
    * @instance
    * @method isRunning
-   * @see {@link ProAct.Flow#start}
-   * @see {@link ProAct.Flow#stop}
    */
   isRunning: function () {
     return this.flowInstance !== null && this.flowInstance !== undefined;
   },
 
   /**
-   * Checks if this ProAct.Flow is paused.
+   * Checks if this `ProAct.Flow` is paused.
    *
-   * @memberof ProAct.Flow
+   * @for ProAct.Flow
    * @instance
    * @method isPaused
-   * @see {@link ProAct.Flow#pause}
-   * @see {@link ProAct.Flow#resume}
    */
   isPaused: function () {
     return this.isRunning() && this.pauseMode;
@@ -315,7 +302,7 @@ P.F.prototype = {
    * Pushes an action to the flow.
    * This method can defer in the flow the same action multiple times.
    * <p>
-   *  ProAct.Flow#defer, ProAct.Flow#enque and ProAct.Flow#add are aliases of this method.
+   *  `defer`, `enque` and `add` are aliases of this method.
    * </p>
    * <p>
    *  If the flow is paused, the action will not be defered.
@@ -323,7 +310,7 @@ P.F.prototype = {
    *
    * TODO Errors should be put in constants!
    *
-   * @memberof ProAct.Flow
+   * @for ProAct.Flow
    * @instance
    * @method push
    * @param {String} queueName
@@ -347,8 +334,6 @@ P.F.prototype = {
    *      </p>
    * @param {Array} args
    *      Arguments to be passed to the action when it is executed.
-   * @see {@link ProAct.Queues#push}
-   * @see {@link ProAct.Flow#isPaused}
    * @throws {Error} <i>Not in running flow!</i>, if there is no action flow instance.
    */
   push: function (queueName, context, action, args) {
@@ -367,13 +352,13 @@ P.F.prototype = {
    *  adding it, its set to be executed later then all the actions that were defered only once, using this method.
    * </p>
    * <p>
-   *  ProAct.Flow#deferOnce, ProAct.Flow#enqueOnce and ProAct.Flow#addOnce are aliases of this method.
+   *  `deferOnce`, `enqueOnce` and `addOnce` are aliases of this method.
    * </p>
    * <p>
    *  If the flow is paused, the action will not be defered.
    * </p>
    *
-   * @memberof ProAct.Flow
+   * @for ProAct.Flow
    * @instance
    * @method pushOnce
    * @param {String} queueName
@@ -397,8 +382,6 @@ P.F.prototype = {
    *      </p>
    * @param {Array} args
    *      Arguments to be passed to the action when it is executed.
-   * @see {@link ProAct.Queues#pushOnce}
-   * @see {@link ProAct.Flow#isPaused}
    * @throws {Error} <i>Not in running flow!</i>, if there is no action flow instance.
    */
   pushOnce: function (queueName, context, action, args) {
@@ -416,20 +399,18 @@ P.F.prototype = {
 };
 
 /**
- * The {@link ProAct.Flow} instance used by ProAct's property updates by default.
+ * The {{#crossLink "ProAct.Flow"}}{{/crossLink}} instance used by ProAct's property updates by default.
  * <p>
  *  It defines only one queue - the default one <i>proq</i>.
- * </p>
- * <p>
- *   It has default error callback that outputs errors to the {@link ProAct.flow.errStream}, if defined.
  * </p>
  * <p>
  *  Override this instance if you are creating a framework or toolset over ProAct.js.
  * </p>
  *
+ * @property flow
  * @type ProAct.Flow
- * @memberof ProAct
- * @static
+ * @for ProAct
+ * @final
  */
 ProAct.flow = new ProAct.Flow(['proq'], {
   err: function (e) {
