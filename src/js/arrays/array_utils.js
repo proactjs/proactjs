1fb140756facd67323a85637fbe17ae3e26a7e9f
meddle <meddlesmerch@gmail.com>
2015-07-08 00:00:13 +0300

    ArrayUtils initialized.

--- src/js/arrays/array_utils.js
+++ src/js/arrays/array_utils.js
@@ -1,2 +1,26 @@
-ProAct.Array.Utils = Pro.A.U = {
+ProAct.ArrayUtils = Pro.AU = {
+
+  /**
+   * Checks if the passed value is instance of the {{#crossLink "ProAct.Array"}}{{/crossLink}} type or not.
+   *
+   * @method isProArray
+   * @param {Object} value The value to check.
+   * @return {Boolean} True if the passed `value` is a ProAct.Array instance.
+   */
+  isProArray: function (value) {
+    return value !== null && P.U.isObject(value) && P.U.isArray(value._array) && value.length !== undefined;
+  },
+
+  /**
+   * Checks if the passed value is a valid array-like object or not.
+   * Array like objects in ProAct.js are plain JavaScript arrays and {{#crossLink "ProAct.Array"}}{{/crossLink}}s.
+   *
+   * @method isArrayObject
+   * @param {Object} value The value to check.
+   * @return {Boolean} True if the passed `value` is an Array or ProAct.Array instance.
+   */
+  isArrayObject: function (value) {
+    return P.U.isArray(value) || P.ArrayUtils.isProArray(value);
+  }
+
 };
